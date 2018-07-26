d0866a7d8b3c5f3e5fcd85010245603e4566fca8
songhaeun <song96244@gmail.com>
2018-07-26 17:11:29 +0900

    sumDivisibleBy()

--- Euler/src/projectEuler/P01.java
+++ Euler/src/projectEuler/P01.java
@@ -1,28 +1,24 @@
 package projectEuler;
 
-// 1000보다 작은 자연수 중에서 3 또는 5의 배수 모두 더하기
-// -> 3의 배수 다 구해서 더하고
-// -> 5의 배수 다 구해서 더한 다음에
-// -> 15의 배수 다 구해서 더하고 빼주기
+// 더 효울적인 코드를 짜보자!
 public class P01{
+	
+	static int target = 999;
+
+	// sumDivisibleBy메소드 생성
+	static int sumDivisibleBy(int i) {
+		// 3 + 6 + 9 + ... + 999 = 3 * (1 + 2 + 3 + ... + 333)
+		// 5 + 10 + 15 + ... + 995 = 5 * (1 + 2 + 3 + ... + 199)
+		
+		int j = 0;
+		j = target / i;
+		
+		return i * j * (j + 1) / 2; 
+	}
 	
 	public static void main(String args[]) {
 		
-		int rsum = 0;
-		
-		for(int i = 0; i < 1000; i++) {
-			if(i % 3 == 0)
-				rsum += i;
-			else if(i % 5 == 0)
-				rsum += i;
-		}
-		// 근데 이렇게 하면 15의 배수들은 if에서 sum3에 더해지니까
-		// else if를 그냥 통과하게 되고
-		// 그럼 굳이 15의 배수들의 합을 따로 구해서 빼주는 작업을 안해도 되는거 아닌가
-		
-		System.out.println(rsum);
+		System.out.println(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15));
 		
 	}
 }
-// 그런데 이렇게 하나하나 확인하는 작업은 시간이 많이 필요함!(숫자의 범위가 늘어날수록)
-// -> 더 효율적인 코드를 짜보자
