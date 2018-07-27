cfa599507e8edccaba1eb02899b017cfacd2ad80
songhaeun <song96244@gmail.com>
2018-07-27 21:34:38 +0900

    3 더 큰 숫자가 주어질 경우

--- Euler/src/projectEuler/P03.java
+++ Euler/src/projectEuler/P03.java
@@ -4,36 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// 600851475143의 소인수 중에서 가장 큰 수 
+// 3 더 큰 숫자가 주어졌을 경우?
+// -> 모든 숫자 n은  n의 제곱근 보다 큰 소인수를 최대 한개 갖는다!
 
-// 2
-// 1을 업데이트 해보자
-// -> 2는 유일하게 짝수인 소수
-// -> 2만 따로 빼내서 다루면 fac을 한번에  +2 할 수 있음(다른 짝수를 무시할 수 있으니까)
+// -> 한 소인수로 나눈 후의 n의 제곱근을 구해서 fac의 한계점으로 쓸 수 있음
+// -> fac이 이 한계점을 넘으면 남은 n이 소수임을 알 수 있음
 public class P03 {
 
 	public static void main(String args[]) {
 		
-		long  evilBigNum = 600851475143L;
+		long  moreEvilBigNum = 600851475143L;
 		int fac = 3; // 이걸 사용하게 된다는 건 인수로 2만 갖는게 아니라는 뜻이되니까(2 이후의 숫자들에 대해서만 조사하면 되니까)
 		int lFac = 0;
-		
-		if (evilBigNum % 2 == 0) {
-			evilBigNum /= 2;
+		double mFac = 0;
+
+		if(moreEvilBigNum % 2 == 0) {
 			lFac = 2;
-			while(evilBigNum % 2 == 0)
-				evilBigNum /= 2;
+			moreEvilBigNum /= 2;
+			while(moreEvilBigNum % 2 == 0)
+				moreEvilBigNum /= 2;
+		}
+		else
+			lFac = 1;
+		
+		mFac = Math.sqrt(moreEvilBigNum);
+		
+		while(moreEvilBigNum > 1 && fac <= mFac) {
+			if(moreEvilBigNum % fac == 0) {
+				lFac = fac;
+				moreEvilBigNum /= fac;
+				while(moreEvilBigNum % fac == 0)
+					moreEvilBigNum /= fac;
+				mFac = Math.sqrt(moreEvilBigNum);			
+			}
+			fac += 2;
 		}
 		
-		while(evilBigNum > 1) {
-			if(evilBigNum % fac == 0) {
-				evilBigNum /= fac;
-				lFac = fac;
-				while(evilBigNum % fac == 0)
-					evilBigNum /= fac;
-			}
-			fac +=2;
-		}
-		System.out.println(lFac);	
+		if(moreEvilBigNum == 1)
+			System.out.println(lFac);
+		else
+			System.out.println(moreEvilBigNum);
 	}
 } 
