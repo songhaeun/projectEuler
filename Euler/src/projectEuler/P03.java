687103a406d32de6f157422ef273fda62c252385
songhaeun <song96244@gmail.com>
2018-07-27 18:25:19 +0900

    2 소인수 분해(2/홀수)

--- Euler/src/projectEuler/P03.java
+++ Euler/src/projectEuler/P03.java
@@ -6,32 +6,33 @@ import java.util.Scanner;
 
 // 600851475143의 소인수 중에서 가장 큰 수 
 
-// 1 가장 단순하게 생각해낼 수 있는 방법(소인수 분해 이용)
-// 주어진 숫자를 n이라고하고 k = 2, 3, 4, 5 ... 
-// k가 n의 인수일 때
-// -> n을 k로 나누고 k를 완전히 쪼개버린 후에 다음 k로 이동하면
-// -> k는 소수가 되고 
-// -> 결과적으로 n은 1이됨
+// 2
+// 1을 업데이트 해보자
+// -> 2는 유일하게 짝수인 소수
+// -> 2만 따로 빼내서 다루면 fac을 한번에  +2 할 수 있음(다른 짝수를 무시할 수 있으니까)
 public class P03 {
 
 	public static void main(String args[]) {
 		
 		long  evilBigNum = 600851475143L;
-		int fac = 2;
+		int fac = 3; // 이걸 사용하게 된다는 건 인수로 2만 갖는게 아니라는 뜻이되니까(2 이후의 숫자들에 대해서만 조사하면 되니까)
 		int lFac = 0;
 		
-		while(evilBigNum>1) {
+		if (evilBigNum % 2 == 0) {
+			evilBigNum /= 2;
+			lFac = 2;
+			while(evilBigNum % 2 == 0)
+				evilBigNum /= 2;
+		}
+		
+		while(evilBigNum > 1) {
 			if(evilBigNum % fac == 0) {
-				lFac = fac;
 				evilBigNum /= fac;
-				while(evilBigNum % fac == 0) {
+				lFac = fac;
+				while(evilBigNum % fac == 0)
 					evilBigNum /= fac;
-				}
-				// 기존 코드의 2번에서 소인수 분해를 이용했을 때는 없었던 부분!
-				// 이 부분이 없으면 fac을 완전히 쪼개버리지 못해서 
-				// evilBigNum에 주어진 숫자가 무엇이냐에 따라 오류의 여지가있음!
 			}
-			fac++;
+			fac +=2;
 		}
 		System.out.println(lFac);	
 	}
